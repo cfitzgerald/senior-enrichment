1a854c6395e628699bb65dea9a6ceb8a80c82c75
Colin FitzGerald <colinclarkfitzgerald@gmail.com>
2017-09-30 09:07:03 -0400

    yet more heroku

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const db = new Sequelize(
   process.env.DATABASE_URL ||
-  'postgres://localhost/sep', {
+  'postgres://localhost:5432/sep', {
     logging: false
   }
 );
